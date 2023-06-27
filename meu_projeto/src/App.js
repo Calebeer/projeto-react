318529d344cac7b008c2e71c185e1f253f94670a
calebeer <sostenesapollo55@gmail.com>
2023-06-26 21:20:34 -0300

    commit

--- meu_projeto/src/App.js
+++ meu_projeto/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa'; 
+
 
 function App() {
+
+  const nome = 'calebe'
+
   return (
     <div className="App">
-      <h1>Olá mundo </h1>
-      
+      <HelloWorld/>
+      <SayMyName nome= "Calebe" />
+      <SayMyName nome= "Apollo" />
+      <SayMyName nome= {nome} />
+      <Pessoa nome="Calebe" idade="12" profissao="Dev" foto="https://via.placeholder.com/150"/>
+
     </div>
   );
 }
