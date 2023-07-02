f4319404c911a1024a9d8a1bc7982a76f1899ce1
calebeer <sostenesapollo55@gmail.com>
2023-07-02 12:25:21 -0300

    primeiro commit do dia

--- meu_projeto/src/components/Form.js
+++ meu_projeto/src/components/Form.js
@@ -1,38 +1,45 @@
+
 import { useState } from 'react';
 
-function Form(){
-    // function cadastrarUsuario(e){
-    //     e.preventDefault()
-    //     console.log('Cadastrou o usuário');
-    // }
-    
-    const [nameInput, setNameInput] = useState('')
-    const [passwordInput, setPasswordInput] = useState('')
 
-    const handleNameInputChange = (event) => setNameInput(event.target.value)
-    const handlePasswordInputChange = (event) => setPasswordInput(event.target.value)
+//  const [nameInput, setNameInput] = useState('')
+   //  const [passwordInput, setPasswordInput] = useState('')
 
-    function dados(event){
-         event.preventDefault()
-         console.log(`email:${nameInput},senha:${passwordInput}`)
-         if(nameInput == 'Calebe' && passwordInput == '8102'){
-            console.log('Logado com sucesso')
-         }else{
-            console.log('Usuário ou senha incorretos');
-         }
-    }
+   //  const handleNameInputChange = (event) => setNameInput(event.target.value)
+   //  const handlePasswordInputChange = (event) => setPasswordInput(event.target.value)
+
+   //  function dados(event){
+   //       event.preventDefault()
+   //       console.log(`email:${nameInput},senha:${passwordInput}`)
+   //       if(nameInput == 'Calebe' && passwordInput == '8102'){
+   //          console.log('Logado com sucesso')
+   //       }else{
+   //          console.log('Usuário ou senha incorretos');
+   //       }
+   //  }
+
+
+   function Form(){
+      function cadastrarUsuario(e){
+          e.preventDefault()
+          console.log(name,password)
+          console.log('Cadastrou o usuário');
+      }
+  
+      const[name, setName] = useState();
+      const[password, setPassword] = useState()
+
 
 
     return(
         <div>
            <h1>Cadastro</h1>
-           <form  >
-            <label>Nome</label>
-            <input type="text" name='name' placeholder="Digite o seu nome" onChange={handleNameInputChange}  />
-            <label>Senha</label>
-            <input type="password" name='password' placeholder="Digite sua senha" onChange={handlePasswordInputChange}  />
-            {/* <input type="submit" value="cadastrar" /> */}
-            <button onClick={dados}  >Cadastrar</button>
+           <form onSubmit={cadastrarUsuario} >
+            <label htmlFor='name' >Nome</label>
+            <input type="text" id='name' name='name' placeholder="Digite o seu nome" onChange={(e) => setName(e.target.value)}  />
+            <label htmlFor='password' >Senha</label>
+            <input type="password" id='password' name='password' placeholder="Digite a sua senha"  onChange={(e) => setPassword(e.target.value)}  />
+            <input type="submit" value="cadastrar" />
            </form>
         </div>
     )
