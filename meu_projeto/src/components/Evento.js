e91cc5f1f97aa516d403be296f114a2f64f8c45f
calebeer <sostenesapollo55@gmail.com>
2023-07-03 00:47:01 -0300

    comiit

--- meu_projeto/src/components/Evento.js
+++ meu_projeto/src/components/Evento.js
@@ -1,17 +1,21 @@
-import HelloWorld from "./HelloWorld";
+import Button from "./evento/Button";
 
-function Evento({ numero }){
+function Evento(){
    function meuEvento(){
-    console.log(`Foi ativado ${numero}`)
+    console.log(`Ativando primeiro evento`)
    }
 
-   function pegar(){
-
+   function segundoEvento(){
+        console.log('Ativando o segundo evento');
    }
    
     return(
         <div>
+
+
             <p>Clique para disparar um evento:</p>
+           <Button event={meuEvento} text="primeiro evento" />
+           <Button event={segundoEvento} text="segundo evento" />
             <button onClick={meuEvento} >Ativar</button>
         </div>
     )
